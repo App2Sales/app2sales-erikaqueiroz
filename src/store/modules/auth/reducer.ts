bf4add0f2828138b8c414e778c9c4c8ebfbb27c4
Evandro Carreira <evandro.carreira@gmail.com>
2020-10-12 20:07:07 -0300

    feat(campos): Tratando campos individualmente

--- src/store/modules/auth/reducer.ts
+++ src/store/modules/auth/reducer.ts
@@ -1,10 +1,11 @@
 import { AuthState, AuthAction } from "./types";
 
-const initialState = {
+const initialState: AuthState = {
   loadingSignInRequest: false,
   isSignedIn: false,
   error: false,
   errorMsg: '',
+  fieldErrors: [],
   token: '',
 };
 
@@ -15,6 +16,7 @@ export default function auth(state = initialState, action: AuthAction): AuthStat
         ...state,
         loadingSignInRequest: true,
         error: false,
+        fieldErrors: [],
         errorMsg: '',
         token: '',
       };
@@ -31,7 +33,7 @@ export default function auth(state = initialState, action: AuthAction): AuthStat
         loadingSignInRequest: false,
         isSignedIn: false,
         error: true,
-        token: '',
+        fieldErrors: action.payload.fieldErrors,
         errorMsg: action.payload.errorMsg,
       };
     default:
--- src/store/modules/auth/types.ts
+++ src/store/modules/auth/types.ts
@@ -4,10 +4,16 @@ import { AnyAction } from 'redux';
 // export type AuthAction = ActionType<typeof actions>;
 export type AuthAction = AnyAction;
 
+export type FieldError = {
+  msg: string;
+  param: string;
+  value: string;
+}
 export interface AuthState {
   readonly loadingSignInRequest: boolean;
   readonly isSignedIn: boolean;
   readonly error: boolean;
   readonly errorMsg: string;
+  readonly fieldErrors: FieldError[];
   readonly token: string;
 }
